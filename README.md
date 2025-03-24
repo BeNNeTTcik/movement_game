fcdb0899c0d53c950ce7dff3834db1c8bce0f4af
BeNNeTTcik <mateuszdampc99@gmail.com>
2025-03-24 22:40:13 +0100

    Gotowe

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # movement_game
 Game
+
+resource 
--- game.py
+++ game.py
@@ -3,56 +3,32 @@ import sys
 #import cv2
 import numpy as np
 #import hailo
-from collision import is_collision
-from lives import lives
 
-#player
-player = 300
-player_speed = 0.5
+# Initialize PyGame
+pygame.init()
 
-#obstacle
-obstacle = 200
-obstacle_speed = 0.2
-
-screen = pygame.display.set_mode((1920, 1080))
+# Set up the game window
+screen_width = 1920
+screen_height = 1080
+background_color = pygame.image.load('resource\\background.jpg')
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Movement")
 
-running = True
-while running:
-    if is_collision(player, 300, obstacle, 300):
-        lives -= 1
-        if lives ==0:   
-            print("Collision!\nGame Over!")
-            running = False
-        else:
-            player = 300
-            obstacle = 200
-    
-    score +=1
-    print("Score: ", score)
-    print("Lives: ", lives)
-        
+# Set the frame rate
+clock = pygame.time.Clock()
+
+# Main game loop
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-            
-    #movement player
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        player -= player_speed
-    if keys[pygame.K_DOWN]:
-        player += player_speed        
-        
-    #movement obstacle
-    obstacle += obstacle_speed
-    if obstacle > 1900 or obstacle <= 0:
-        obstacle_speed *= -1
-            
-    screen.fill((0, 0, 0))
-    
-    pygame.draw.rect(screen, (255, 255, 255), (100, player, 50, 50))
-    pygame.draw.rect(screen, (255, 0, 0), (obstacle, 300, 50, 50))
-    
-    pygame.display.update()
-pygame.quit()
+            pygame.quit()
+            sys.exit()
 
+    # Fill the screen with a color (black in this case)
+    screen.fill((0, 0, 0))
+
+    # Update the display
+    pygame.display.flip()
+
+    # Cap the frame rate at 60 frames per second
+    clock.tick(60)
