b3897660dd5b8660454d216277873c4128246ef6
BeNNeTTcik <mateuszdampc99@gmail.com>
2025-03-30 20:15:00 +0200

    Gotowe

--- game.py
+++ game.py
@@ -10,12 +10,30 @@ pygame.init()
 # Set up the game window
 screen_width = 1792
 screen_height = 1024
-background_color = pygame.image.load("resources\\background.jpg")
+background = pygame.image.load("resources\\background.jpg")
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Movement")
 
+# Platform dimensions
+platform_width = 200
+platform_height = 20
+platform = pygame.Rect(350, 500, platform_width, platform_height)
+
+#player dimensions
+player_width = 50
+player_height = 50
+
+#layers 
+back_layer = pygame.Surface((screen_width, screen_height))
+back_layer.fill(background.get_at((0, 0)))  # Fill with the background color
+mid_layer = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
+top_layaer = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
+
 # Set the frame rate
 clock = pygame.time.Clock()
+
+#screen.blit(background, (0, 0))
+pygame.display.flip()
 
 # Main game loop
 while True:
@@ -24,11 +42,12 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # Fill the screen with a color (black in this case)
-    #screen.fill((0, 0, 0))
+      
 
     # Update the display
     pygame.display.flip()
 
     # Cap the frame rate at 60 frames per second
     clock.tick(60)
+
+pygame.quit()
